03317e3b6c0480bfc6b2f92e20e12bcd6225707e
James <wishthiswaswow@gmail.com>
2022-02-07 17:06:53 +0000

    JB: initial commit

---
+++ data_types.py
@@ -0,0 +1,18 @@
+#word1 = "Good"
+#word2 = "Day"
+#word3 = "James"
+
+#sentence = word1 + " " + word2 + " " + word3
+
+#print(sentence)
+
+number1 = input("Enter a whole number: ")
+number2 = input("Enter a decimal number: ")
+
+int_num = int(number1)
+flt_num = float(number2)
+round_num = int(round(flt_num))
+
+print(number1)
+print(number2)
+print(round_num)
---
+++ intro.py
@@ -0,0 +1,20 @@
+#print("Hello world!")
+
+#print("Hello " + "James " + "Bolton!")
+
+first_name = input("Hello, what is your first name? ")
+last_name = input("And what is your is your last name? ")
+print("Hello " + first_name + " " + last_name + "!")
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, " + ", number2, " = ", answer)
+
+number1 = float(input("Please enter the first number: "))
+number2 = float(input("Please enter the second number: "))
+answer = number1 + number2
+
+print(number1, "+", number2, "=", answer)
+#
